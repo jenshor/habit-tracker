ac37b48ff7686365b9e4a8cfee50773e2a509c10
Jens Horstmann <jens@firecrown.pro>
2021-01-17 13:18:59 +0100

    added user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,11 +1,16 @@
-class User {
-  String id;
-  String name;
-  String email;
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  final String id;
+  final String name;
+  final String email;
 
   User({
     this.id,
     this.name,
     this.email,
   });
+
+  @override
+  List<Object> get props => [id, name, email];
 }
