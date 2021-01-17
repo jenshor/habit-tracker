be83a819d108b67ca9bb548a9643148701868e17
Jens Horstmann <jens@firecrown.pro>
2021-01-17 08:46:09 +0100

    added enableFlutterDriverExtension to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import "package:flutter_driver/driver_extension.dart";
 
-void main() => runApp(App());
+void main() {
+  enableFlutterDriverExtension();
+  runApp(App());
+}
 
 class App extends StatelessWidget {
   @override
