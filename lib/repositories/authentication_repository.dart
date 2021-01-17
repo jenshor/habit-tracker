1ade28756324b1e8b4d20e296d26c9c39ed8a77a
Jens Horstmann <jens@firecrown.pro>
2021-01-17 13:26:44 +0100

    added authentication_repository from bloc documentation

---
+++ lib/repositories/authentication_repository.dart
@@ -0,0 +1,89 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:habit_tracker/exceptions/exceptions.dart';
+import 'package:meta/meta.dart';
+import 'package:habit_tracker/models/user.dart' as model;
+
+class AuthenticationRepository {
+  AuthenticationRepository({
+    FirebaseAuth firebaseAuth,
+    GoogleSignIn googleSignIn,
+  })  : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
+        _googleSignIn = googleSignIn ?? GoogleSignIn.standard();
+
+  final FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
+
+  Stream<model.User> get user {
+    return _firebaseAuth.authStateChanges().map((firebaseUser) {
+      return firebaseUser != null ? firebaseUser.toUser : null;
+    });
+  }
+
+  Future<void> signUp({
+    @required String email,
+    @required String password,
+  }) async {
+    assert(email != null && password != null);
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on Exception {
+      throw SignUpFailure();
+    }
+  }
+
+  Future<void> logInWithGoogle() async {
+    try {
+      final googleUser = await _googleSignIn.signIn();
+      final googleAuth = await googleUser.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      await _firebaseAuth.signInWithCredential(credential);
+    } on Exception {
+      throw LogInWithGoogleFailure();
+    }
+  }
+
+  Future<void> logInWithEmailAndPassword({
+    @required String email,
+    @required String password,
+  }) async {
+    assert(email != null && password != null);
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on Exception {
+      throw LogInWithEmailAndPasswordFailure();
+    }
+  }
+
+  Future<void> logOut() async {
+    try {
+      await Future.wait([
+        _firebaseAuth.signOut(),
+        _googleSignIn.signOut(),
+      ]);
+    } on Exception {
+      throw LogOutFailure();
+    }
+  }
+}
+
+extension on User {
+  model.User get toUser {
+    return model.User(
+      id: uid,
+      email: email,
+      name: displayName,
+      // TODO think about implementing this later
+      // photo: photoURL,
+    );
+  }
+}
