acacd88640af857e3abc38f34ec1e0e7d4b04a7a
Jens Horstmann <jens@firecrown.pro>
2021-01-31 13:41:40 +0100

    changed ModifyHabitPage to StatelessWidget

--- lib/pages/modify_habit_page.dart
+++ lib/pages/modify_habit_page.dart
@@ -1,19 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:habit_tracker/blocs/habit_bloc/habit_bloc.dart';
 import 'package:habit_tracker/forms/widgets/modify_habit_form.dart';
-import 'package:habit_tracker/models/habit.dart';
 import 'package:habit_tracker/widgets/custom_scaffold.dart';
 import 'package:habit_tracker/widgets/spacer_box.dart';
 
-class ModifyHabitPage extends StatefulWidget {
-  @override
-  _ModifyHabitPageState createState() => _ModifyHabitPageState();
-}
-
-class _ModifyHabitPageState extends State<ModifyHabitPage> {
-  TextEditingController nameController = TextEditingController();
-
+class ModifyHabitPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
