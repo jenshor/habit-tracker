5abee78cb760a6f968f022b22e61a9a308cf04a8
Jens Horstmann <jens@firecrown.pro>
2021-02-01 18:24:29 +0100

    changed app back button to black color

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,11 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          appBarTheme: AppBarTheme(elevation: 0, color: Colors.transparent),
+          appBarTheme: AppBarTheme(
+            elevation: 0,
+            color: Colors.transparent,
+            iconTheme: IconThemeData(color: Colors.black),
+          ),
           scaffoldBackgroundColor: Color(0xFFF2F1F5),
           textTheme: GoogleFonts.montserratTextTheme().apply(
               bodyColor: Colors.black,
